2d29f0931fb42ad4ad3ff7a4bc1753897612340a
gaofeng222 <596642721@qq.com>
2023-02-16 21:51:30 +0800

    打包更目录修改

---
+++ precache-manifest.cef465bff34cd88fe6452e1e238df619.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9d604300c8d6bd5677",
+    "url": "/vue3musicapp/css/app.6dd0ba7c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue3musicapp/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3c35d3049f4bdc0d5bb57d790f22bf88",
+    "url": "/vue3musicapp/index.html"
+  },
+  {
+    "revision": "5cdc3e3fa37a2b9d63d9",
+    "url": "/vue3musicapp/js/about.78b93816.js"
+  },
+  {
+    "revision": "4c9d604300c8d6bd5677",
+    "url": "/vue3musicapp/js/app.e88d7088.js"
+  },
+  {
+    "revision": "9b273d0a5a4521cfe634",
+    "url": "/vue3musicapp/js/chunk-vendors.887eb325.js"
+  },
+  {
+    "revision": "58f1c09268aa1be9cba3da542b25ba7d",
+    "url": "/vue3musicapp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue3musicapp/robots.txt"
+  }
+]);
